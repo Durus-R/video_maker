e43a3851117dd88b40c664574e35ce0c3efba107
Durus-R <smartpower0105@gmail.com>
2023-03-25 16:36:45 +0100

    Fix requirements.txt and disable autoinstallation over network

--- autoinstall.ps1
+++ autoinstall.ps1
@@ -4,28 +4,15 @@ if (!(Get-Command pipenv -ErrorAction SilentlyContinue)) {
     pip install pipenv
 }
 
-# Download the repository as a ZIP file
-Invoke-WebRequest -Uri 'https://github.com/Durus-R/video_maker/archive/master.zip' -OutFile "$env:USERPROFILE\Downloads\repo.zip"
-
-# Expand the ZIP file to the home directory
-Expand-Archive "$env:USERPROFILE\Downloads\repo.zip" -DestinationPath $env:USERPROFILE
-
-# Rename the extracted folder to "repo"
-Rename-Item -Path "$env:USERPROFILE\video_maker-master" -NewName "slideshow_maker"
-
-# Change to the repo directory
-cd ~\repo
-
 # Create a new pipenv environment using the requirements.txt file
-pipenv install
+python -m pipenv install
 
 # Create a batch file to activate the environment and run main.py
 $batFilePath = "$env:USERPROFILE\Desktop\slideshow_maker.bat"
 @"
 @echo off
-cd $env:USERPROFILE\slideshow_maker
-call pipenv shell
-python main.py
+cd $PWD
+call python -m pipenv run python main.py
 "@ | Set-Content -Path $batFilePath -Encoding ASCII
 
 # Output the path to the batch file
